8a8368b8a827dbbcf51caa2cd7a230a5923f505b
Drew <dlazzeri1@gmail.com>
2021-02-08 22:38:08 -0700

    fix: Remove typescript

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { IonicVue } from '@ionic/vue';
+import Vuex from 'vuex';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -23,6 +24,7 @@ import '@ionic/vue/css/display.css';
 import './theme/variables.css';
 
 const app = createApp(App)
+  .use(Vuex)
   .use(IonicVue)
   
 app.mount('#app');
