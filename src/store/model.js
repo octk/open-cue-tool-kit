b34c64c0cd8e0f77a85e7c9a9a0fddd878274b6f
Drew <dlazzeri1@gmail.com>
2021-02-09 00:00:23 -0700

    Revert "Revert "feat(ui): Made qad casting page""
    This reverts commit 8dcd04ad8ada92d22bcf50ad757eaab0b89ef930.

--- src/store/model.js
+++ src/store/model.js
@@ -1,10 +1,22 @@
 export default {
   state: {
-    playName: "Macbeth"
+    playName: "Macbeth",
+    invitationLink: "cuecannon.com/asdf",
+    cast: [
+      { name: "Drew", role: "First Witch" },
+      { name: "Shokai", role: "Second Witch" },
+      { name: "Daniel", role: "Third Witch" }
+    ]
   },
   getters: {
     PLAY_NAME(state) {
       return state.playName;
+    },
+    INVITATION_LINK(state) {
+      return state.invitationLink;
+    },
+    CAST(state) {
+      return state.cast;
     }
   }
 };
