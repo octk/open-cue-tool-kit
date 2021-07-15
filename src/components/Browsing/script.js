f8c462f008b22688231a9a880f80f9434464eb6a
Drew <dlazzeri1@gmail.com>
2021-07-15 07:15:46 -0600

    wip: sections from cached plays

--- src/components/Browsing/script.js
+++ src/components/Browsing/script.js
@@ -4,14 +4,35 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Browsing",
   components,
+  data: () => ({ selectedPlay: null }),
   computed: {
     ...mapGetters({
       availablePlays: "DIR_PLAYS"
-    })
+    }),
+    availableSections() {
+      if (!this.selectedPlay) return [];
+      const sections = this.selectedPlay.lines.reduce((acc, { l: line }) => {
+        const [act, scene] = line.split(".");
+        acc[act] = `Act ${act}`;
+        acc[scene] = `Act ${act} Scene ${scene}`;
+        return acc;
+      }, {});
+      sections["All"] = "All"; // Whole play
+      return sections;
+    }
   },
   methods: {
+    selectPlay(play) {
+      this.selectedPlay = play;
+    },
+    deselectPlay() {
+      this.selectPlay = null;
+    },
+    selectSection(section) {
+      this.directPlay(this.selectedPlay, section);
+    },
     ...mapActions({
-      selectPlay: "DIR_SELECT_PLAY"
+      directPlay: "DIR_SELECT_PLAY"
     })
   }
 };
